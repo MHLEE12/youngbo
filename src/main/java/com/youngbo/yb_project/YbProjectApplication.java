7c9e9fcb42c59256e33f42074281c37e4c7314d1
MHLEE12 <coolmh12@gmail.com>
2024-12-13 22:49:28 +0900

    project init

---
+++ src/main/java/com/youngbo/yb_project/YbProjectApplication.java
@@ -0,0 +1,13 @@
+package com.youngbo.yb_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YbProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YbProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/youngbo/yb_project/YbProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youngbo.yb_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YbProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
