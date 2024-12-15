5731539c614f79e17da588d9273c5f9f8b6bb24b
MHLEE12 <coolmh12@gmail.com>
2024-12-15 20:57:37 +0900

    Feat: main 페이지 추가
    메인페이지 추가했습니다.

---
+++ src/main/java/com/youngbo/yb_project/controller/PageController.java
@@ -0,0 +1,15 @@
+package com.youngbo.yb_project.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class PageController {
+
+    @GetMapping("/")
+    public String main() {
+        return "/index";
+    }
+}
